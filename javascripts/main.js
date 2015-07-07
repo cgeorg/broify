720c49fd0b2609a092a67a4225d2b427d09693cd
Chris Marasti-Georg <c.marastigeorg@gmail.com>
2015-07-06 21:14:07 -0400

    Update to latest version

--- javascripts/main.js
+++ javascripts/main.js
@@ -67,10 +67,10 @@
 	function broify(str) {
 	    return str && [
 	    //Special cases where 2 letters really should be turned into br
-	    [/sco/gi, 'bro'], [/[A-Z]ro/g, 'Bro'], [/[a-z]ro/g, 'bro'], [/((Br)|(^|.)[BCDFGHJKLMNPQRSTVWXYZ](o+))/g, function ($0, $1, $2, $3, $4) {
-	        return $2 ? $0 : $3 + 'Br' + $4;
-	    }], [/(([Bb]r)|(^|.)[bcdfghjklmnprstvwxyz](o+))/g, function ($0, $1, $2, $3, $4) {
-	        return $2 ? $0 : $3 + 'br' + $4;
+	    [/sco/gi, 'bro'], [/who/gi, 'bro'], [/kno/gi, 'bro'], [/clo/gi, 'bro'], [/[BCDFGHJKLMNPQRSTVWXYZ]ro/g, 'Bro'], [/[bcdfghjklmnprstvwxyz]ro/g, 'bro'], [/((Br)|[BCDFGHJKLMNPQRSTVWXYZ](o+))/g, function ($0, $1, $2, $3) {
+	        return $2 ? $0 : 'Br' + $3;
+	    }], [/(([Bb]r)|[bcdfghjklmnprstvwxyz](o+))/g, function ($0, $1, $2, $3) {
+	        return $2 ? $0 : 'br' + $3;
 	    }], [/(\b)o/g, '$1bro'], [/(\b)O/g, '$1Bro'], [/(br)[aeiu]/gi, '$1o']].reduce(function (str, r) {
 	        return str.replace(r[0], r[1]);
 	    }, str);
