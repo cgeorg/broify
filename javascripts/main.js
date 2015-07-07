10d8a718f2e390e6ac59be09f5f6051493b0394a
Chris Marasti-Georg <c.marastigeorg@gmail.com>
2015-07-06 22:10:52 -0400

    Upgraded to 1.2

--- javascripts/main.js
+++ javascripts/main.js
@@ -67,7 +67,7 @@
 	function broify(str) {
 	    return str && [
 	    //Special cases where 2 letters really should be turned into br
-	    [/sco/gi, 'bro'], [/who/gi, 'bro'], [/kno/gi, 'bro'], [/clo/gi, 'bro'], [/[BCDFGHJKLMNPQRSTVWXYZ]ro/g, 'Bro'], [/[bcdfghjklmnprstvwxyz]ro/g, 'bro'], [/((Br)|[BCDFGHJKLMNPQRSTVWXYZ](o+))/g, function ($0, $1, $2, $3) {
+	    [/sco/gi, 'bro'], [/who/gi, 'bro'], [/kno/gi, 'bro'], [/clo/gi, 'bro'], [/sho/gi, 'bro'], [/[BCDFGHJKLMNPQRSTVWXYZ]ro/g, 'Bro'], [/[bcdfghjklmnprstvwxyz]ro/g, 'bro'], [/((Br)|[BCDFGHJKLMNPQRSTVWXYZ](o+))/g, function ($0, $1, $2, $3) {
 	        return $2 ? $0 : 'Br' + $3;
 	    }], [/(([Bb]r)|[bcdfghjklmnprstvwxyz](o+))/g, function ($0, $1, $2, $3) {
 	        return $2 ? $0 : 'br' + $3;
